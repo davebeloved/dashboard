444e2e4bc7a8f2192ef1da5642b1643bc370eda2
DAVIDSON IDADAKPOYE <davebelovedcity@gmail.com>
2023-09-15 15:26:56 +0100

    new update

--- src/components/BuyerProfilePieChart.jsx
+++ src/components/BuyerProfilePieChart.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from 'recharts'
+import { PieChart, Cell, ResponsiveContainer, Legend } from 'recharts'
+import { useStateContext } from '../context/contextProvider'
+import { Chart as ChartJs, BarElement, CategoryScale, LinearScale, ArcElement } from 'chart.js'
+import { Pie } from 'react-chartjs-2'
 
 const data = [
     { name: 'Income', value: 540 },
@@ -23,14 +26,31 @@ const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, per
 }
 
 export default function BuyerProfilePieChart() {
+    ChartJs.register(BarElement, ArcElement, LinearScale, CategoryScale)
+    const { pillars } = useStateContext()
+
+    const datas = {
+        labels: pillars.map((pillar) => pillar.pillarname),
+        datasets: [
+            {
+                label: 'Projects',
+                backgroundColor: pillars.map((colors) => colors.color),
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+                hoverBackgroundColor: pillars.map((colors) => colors.color),
+                // hoverBorderColor: 'rgba(75, 192, 192, .75)',
+                data: [2, 4, 4, 3, 1, 5]
+            }
+        ]
+    }
     return (
         <div className="w-[20rem] h-[22rem] ml-28 lg:ml-0 bg-white p-4 rounded-sm border border-gray-200 flex flex-col">
             <strong className="text-gray-700 font-medium"></strong>
             <div className="mt-3 w-full flex-1 text-xs">
-                <ResponsiveContainer width="100%" height="100%">
+                {/* <ResponsiveContainer width="100%" height="100%">
                     <PieChart width={400} height={300}>
                         <Pie
-                            data={data}
+                            data={pillars}
                             cx="50%"
                             cy="45%"
                             labelLine={false}
@@ -39,13 +59,14 @@ export default function BuyerProfilePieChart() {
                             fill="#FF8042"
                             dataKey="value"
                         >
-                            {data.map((_, index) => (
+                            {pillars.map((_, index) => (
                                 <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                             ))}
                         </Pie>
                         {/* <Legend /> */}
-                    </PieChart>
-                </ResponsiveContainer>
+                {/* </PieChart> */}
+                {/* </ResponsiveContainer> */}
+                <Pie data={datas} />
             </div>
         </div>
     )
